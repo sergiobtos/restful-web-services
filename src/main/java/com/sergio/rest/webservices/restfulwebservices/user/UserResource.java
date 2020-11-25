d21b28ebf818235eb6514b0dd66fd30cde27d3de
Sergio Silva <sergiobtos@hotmail.com>
2020-11-25 18:32:27 -0500

    initial commit

---
+++ src/main/java/com/sergio/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,42 @@
+package com.sergio.rest.webservices.restfulwebservices.user;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+public class UserResource {
+	
+	@Autowired
+	private UserDaoService service;
+	
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers(){
+		return service.findAll();
+	}
+	
+	@GetMapping("/users/{id}")
+	public User retrieveUser(@PathVariable int id) {
+		User user = service.findOne(id);
+		if(user == null) {
+			throw new UserNotFoundException("id-"+id);
+		}
+		return user;
+	}
+	
+	@PostMapping("/users")
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
+		User savedUser = service.save(user);
+		
+		URI location = ServletUriComponentsBuilder
+			.fromCurrentRequest()
+			.path("/{id}")
+			.buildAndExpand(savedUser.getId()).toUri();
+		return ResponseEntity.created(location).build();
+	}
+
+}
