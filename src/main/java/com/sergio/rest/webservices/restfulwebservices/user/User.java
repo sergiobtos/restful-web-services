ba61218fc840851b7e534a6c74ce6927e83c665e
Sergio Silva <sergiobtos@hotmail.com>
2020-11-26 16:09:39 -0500

    v6

--- src/main/java/com/sergio/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/sergio/rest/webservices/restfulwebservices/user/User.java
@@ -3,20 +3,33 @@ package com.sergio.rest.webservices.restfulwebservices.user;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
+import java.util.List;
 
 @ApiModel(description="All details about the users")
+@Entity
 public class User {
 	
+	@Id
+	@GeneratedValue
 	private Integer id;
+	
 	@Size(min=2, message = "Name should have at least 2 characters")
 	@ApiModelProperty(notes="Name should have at least 2 characters")
 	private String name;
+	
 	@Past
 	@ApiModelProperty(notes="Birth Date should be in the past")
 	private Date birthDate;
+	
+	@OneToMany(mappedBy="user")
+	private List<Post> posts;
 	
 	protected User() {}
 	
@@ -44,6 +57,14 @@ public class User {
 	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
 	}
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
